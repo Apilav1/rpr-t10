7eb08449fa2b270599ab6aad53816b3d723049c0
Apilav1 <apilav1@etf.unsa.ba>
2018-12-25 11:19:36 +0100

    implementacija main klase

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -8,6 +8,25 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String ispisiGradove(){
+        String ispis="";
+        ArrayList<Grad> g = GeografijaDAO.getInstance().gradovi();
+        for(Grad grad: g){
+            ispis += grad.getNaziv()+ " (" + grad.getDrzava().getNaziv() + ") " + " - " +grad.getBrojStanovnika() +"\n";
+        }
+        return ispis;
+    }
+    public static void glavniGrad(){
+        String drzava;
+        System.out.println("Unesi naziv drzave: ");
+        Scanner ulaz = new Scanner(System.in);
+        drzava = ulaz.next();
+        Grad g = GeografijaDAO.getInstance().glavniGrad(drzava);
+        if(g != null)
+            System.out.println("Glavni grad drzave "+drzava+" je "+g.getNaziv());
+        else
+            System.out.println("Nepostojeca drzava");
+    }
     public static void main(String[] args) {
         /*System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();*/
@@ -18,8 +37,8 @@ public class Main {
            /* Class.forName("org.sqlite.JDBC");
             String url = "jdbc:sqlite:C:/Users/Adi Pilav/Desktop/sqlitee/baza.db";
             conn = DriverManager.getConnection(url);*/
-           GeografijaDAO.getInstance();
-
+            glavniGrad();
+            ispisiGradove();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
